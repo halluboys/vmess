4dd82ec7bf007b713f9749d549eb42119c37702e
halluboys <87511503+halluboys@users.noreply.github.com>
2024-05-23 17:04:44 +0700

    Update m-trojan.sh

--- menu/m-trojan.sh
+++ menu/m-trojan.sh
@@ -5,6 +5,7 @@ echo -e "\E[0;100;33m      • TROJAN MENU •          \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e " [\e[36m•1\e[0m] Create Account Trojan "
+echo -e " [\e[36m•1\e[0m] Create Account TrojanGo "
 echo -e " [\e[36m•2\e[0m] Trial Account Trojan "
 echo -e " [\e[36m•3\e[0m] Extending Account Trojan "
 echo -e " [\e[36m•4\e[0m] Delete Account Trojan "
@@ -20,10 +21,11 @@ read -p " Select menu : " opt
 echo -e ""
 case $opt in
 1) clear ; add-tr ;;
-2) clear ; trialtrojan ;;
-3) clear ; renew-tr ;;
-4) clear ; del-tr ;;
-5) clear ; cek-tr ;;
+2) clear ; addtrgo ;;
+3) clear ; trialtrojan ;;
+4) clear ; renew-tr ;;
+5) clear ; del-tr ;;
+6) clear ; cek-tr ;;
 0) clear ; menu ;;
 x) exit ;;
 *) echo "SALAH TEKAN" ; sleep 1 ; m-trojan ;;
