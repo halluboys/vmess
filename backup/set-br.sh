5aff2134c0ba2e6c3d4f95e177d8e152475c6f8d
halluboys <87511503+halluboys@users.noreply.github.com>
2024-05-28 08:05:05 +0700

    Update set-br.sh

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -2,6 +2,7 @@
 # My Telegram : https://t.me/haluboy
 # ==========================================
 # Color
+
 RED='\033[0;31m'
 NC='\033[0m'
 GREEN='\033[0;32m'
@@ -10,6 +11,64 @@ BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
+color1='\e[031;1m'
+color2='\e[34;1m'
+color3='\e[0m'
+DF='\e[39m'
+Bold='\e[1m'
+Blink='\e[5m'
+yell='\e[33m'
+red='\e[1;31m'
+green='\e[1;32m'
+blue='\e[1;34m'
+PURPLE='\e[1;95m'
+CYAN='\e[1;36m'
+Lred='\e[1;91m'
+Lgreen='\e[92m'
+Lyellow='\e[93m'
+white='\e[1;37m'
+NC='\e[0m'
+tyblue='\e[1;36m'
+
+MYIP=$(wget -qO- ifconfig.me/ip);
+#########################
+IZIN=$(curl -sS https://raw.githubusercontent.com/halluboys/perizinan/main/main/allow | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+echo -e "\e[31mIJIN DULU NGENTOT!\e[0m"
+exit 0
+fi
+#EXPIRED
+expired=$(curl -sS https://raw.githubusercontent.com/halluboys/perizinan/main/main/allow | grep $MYIP | awk '{print $3}')
+echo $expired > /root/expired.txt
+today=$(date -d +1day +%Y-%m-%d)
+while read expired
+do
+	exp=$(echo $expired | curl -sS https://raw.githubusercontent.com/halluboys/perizinan/main/main/allow | grep $MYIP | awk '{print $3}')
+	if [[ $exp < $today ]]; then
+		Exp2="\033[1;31mExpired\033[0m"
+        else
+        Exp2=$(curl -sS https://raw.githubusercontent.com/halluboys/perizinan/main/main/allow | grep $MYIP | awk '{print $3}')
+	fi
+done < /root/expired.txt
+rm /root/expired.txt
+Name=$(curl -sS https://raw.githubusercontent.com/halluboys/perizinan/main/main/allow | grep $MYIP | awk '{print $2}')
+clear
+# VPS Information
+Checkstart1=$(ip route | grep default | cut -d ' ' -f 3 | head -n 1);
+if [[ $Checkstart1 == "venet0" ]]; then
+    clear
+	  lan_net="venet0"
+    typevps="OpenVZ"
+else
+    clear
+		lan_net="eth0"
+    typevps="KVM"
+fi
+clear
+
 # ==========================================
 #Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
