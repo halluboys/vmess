b58313f7f6117dbff315b5ceed3e4bddbfb2fb0b
halluboys <87511503+halluboys@users.noreply.github.com>
2024-05-30 23:03:29 +0700

    Update restore.sh

--- backup/restore.sh
+++ backup/restore.sh
@@ -25,21 +25,7 @@ rm -f backup.zip
 sleep 1
 echo Start Restore
 cd /root/backup
-cp passwd /etc/
-cp group /etc/
-cp shadow /etc/
-cp gshadow /etc/
-# cp -r wireguard /etc/
-# cp chap-secrets /etc/ppp/
-# cp passwd1 /etc/ipsec.d/passwd
-# cp ss.conf /etc/shadowsocks-libev/ss.conf
-# cp -r akbarstorevpn /var/lib/
-# cp -r sstp /home/
 cp -r config.json /etc/xray
-# cp -r trojan-go /etc/
-# cp -r shadowsocksr /usr/local/
-cp -r public_html /home/vps/
-cp crontab /etc/
 strt
 rm -rf /root/backup
 rm -f backup.zip
