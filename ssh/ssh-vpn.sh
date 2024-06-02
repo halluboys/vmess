eb70ebbc03d32db7d3e928135a046347463ea61b
halluboys <87511503+halluboys@users.noreply.github.com>
2024-06-02 22:50:12 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -339,22 +339,22 @@ wget -O m-vmess "https://raw.githubusercontent.com/halluboys/vmess/main/menu/m-v
 wget -O m-vless "https://raw.githubusercontent.com/halluboys/vmess/main/menu/m-vless.sh"
 wget -O running "https://raw.githubusercontent.com/halluboys/vmess/main/menu/running.sh"
 wget -O clearcache "https://raw.githubusercontent.com/halluboys/vmess/main/menu/clearcache.sh"
-wget -O m-ssws "https://raw.githubusercontent.com/halluboys/vmess/main/menu/m-ssws.sh"
-wget -O m-trojan "https://raw.githubusercontent.com/halluboys/vmess/main/menu/m-trojan.sh"
+#wget -O m-ssws "https://raw.githubusercontent.com/halluboys/vmess/main/menu/m-ssws.sh"
+#wget -O m-trojan "https://raw.githubusercontent.com/halluboys/vmess/main/menu/m-trojan.sh"
 
 # menu ssh ovpn
-wget -O m-sshovpn "https://raw.githubusercontent.com/halluboys/vmess/main/menu/m-sshovpn.sh"
-wget -O usernew "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/usernew.sh"
-wget -O trial "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/trial.sh"
-wget -O renew "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/renew.sh"
-wget -O hapus "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/hapus.sh"
-wget -O cek "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/cek.sh"
-wget -O member "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/member.sh"
-wget -O delete "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/delete.sh"
-wget -O autokill "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/autokill.sh"
-wget -O ceklim "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/ceklim.sh"
-wget -O tendang "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/tendang.sh"
-wget -O sshws "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/sshws.sh"
+#wget -O m-sshovpn "https://raw.githubusercontent.com/halluboys/vmess/main/menu/m-sshovpn.sh"
+#wget -O usernew "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/usernew.sh"
+#wget -O trial "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/trial.sh"
+#wget -O renew "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/renew.sh"
+#wget -O hapus "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/hapus.sh"
+#wget -O cek "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/cek.sh"
+#wget -O member "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/member.sh"
+#wget -O delete "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/delete.sh"
+#wget -O autokill "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/autokill.sh"
+#wget -O ceklim "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/ceklim.sh"
+#wget -O tendang "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/tendang.sh"
+#wget -O sshws "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/sshws.sh"
 
 # menu system
 wget -O m-system "https://raw.githubusercontent.com/halluboys/vmess/main/menu/m-system.sh"
@@ -368,7 +368,7 @@ wget -O speedtest "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/sp
 wget -O auto-reboot "https://raw.githubusercontent.com/halluboys/vmess/menu/auto-reboot.sh"
 wget -O restart "https://raw.githubusercontent.com/halluboys/vmess/main/menu/restart.sh"
 wget -O bw "https://raw.githubusercontent.com/halluboys/vmess/main/menu/bw.sh"
-wget -O m-tcp "https://raw.githubusercontent.com/halluboys/vmess/main/menu/tcp.sh"
+#wget -O m-tcp "https://raw.githubusercontent.com/halluboys/vmess/main/menu/tcp.sh"
 wget -O m-backup "https://raw.githubusercontent.com/halluboys/vmess/main/menu/m-backup.sh"
 wget https://raw.githubusercontent.com/halluboys/vmess/main/encrypt/epro && chmod +x epro
 # change port
@@ -377,9 +377,9 @@ wget https://raw.githubusercontent.com/halluboys/vmess/main/encrypt/epro && chmo
 #wget -O port-tr "https://raw.githubusercontent.com/halluboys/Vmess/port/port-tr.sh"
 
 
-wget -O xp "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/xp.sh"
+#wget -O xp "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/xp.sh"
 #wget -O asu "https://raw.githubusercontent.com/halluboys/Vmess/asu.sh"
-wget -O sshws "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/sshws.sh"
+#wget -O sshws "https://raw.githubusercontent.com/halluboys/vmess/main/ssh/sshws.sh"
 
 chmod +x menu
 chmod +x m-vmess
